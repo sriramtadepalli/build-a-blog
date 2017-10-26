e3d11885d1c920c7068681d480979281683fc715
unknown <sriram.tadepalli@gmail.com>
2017-10-26 17:27:28 -0500

    Just demonstrated Build a Blog to Lynn

--- main.py
+++ main.py
@@ -27,6 +27,10 @@ def index():
     if request.method == 'POST':
         title_name = request.form['title']
         body_name = request.form['body']
+
+        if not title_name or not body_name:
+            return render_template('newpost.html',title_error="The title is empty.", body_error = "The body is empty.")            
+
         newpost = Blog(title_name, body_name)
         db.session.add(newpost)
         db.session.commit()
@@ -41,11 +45,16 @@ def blog():
  
     id = request.args.get("id")
 
-    if id:
+    if id:  
 
-        blog_var =  Blog.query.filter_by(id = id)
+        print("In Id................... id.....")
 
-        return render_template('blog_post.html',title="Build a Blog", blog = blog_var)
+        blog_var =  Blog.query.filter_by(id=id).first()
+        #blog_var = Blog.query.get(id=id)
+
+        print("The id we got from the database is ", blog_var)
+
+        return render_template('blog_post.html',title="Build a Blog", blog_var = blog_var)
 
     else:
 
