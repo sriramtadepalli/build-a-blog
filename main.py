58769bdf0db05d2b154241018c9cf033bdb442ce
unknown <sriram.tadepalli@gmail.com>
2017-10-28 12:40:15 -0500

    Till 12:40 pm Sat Oct 28 2017

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -13,12 +13,26 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(200))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-    def __init__(self, title, body): # Blog()
+    def __init__(self, title, body): 
         self.title = title
         self.body = body
+        self.owner = owner 
 
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(200))
+    blogs = db.relationship('Blog', backref='owner')
+    #blogs
+
+
+    def __init__(self, username, password): 
+        self.username = username
+        self.password = password
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
@@ -27,11 +41,12 @@ def index():
     if request.method == 'POST':
         title_name = request.form['title']
         body_name = request.form['body']
+        owner_id_name = request.form['owner']  # Get owner from your session instead of owner field that doesen't exist.
 
         if not title_name or not body_name:
             return render_template('newpost.html',title_error="The title is empty.", body_error = "The body is empty.")            
 
-        newpost = Blog(title_name, body_name)
+        newpost = Blog(title_name, body_name, owner_id_name)
         db.session.add(newpost)
         db.session.commit()
 
