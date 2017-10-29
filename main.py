9382e572f00e9a8a889c769c771873129617af21
unknown <sriram.tadepalli@gmail.com>
2017-10-28 21:47:44 -0500

    End of day Saturday Oct 28

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -37,16 +37,16 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register']
-    if request.endpoint not in allowed_routes and 'email' not in session:
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
 @app.route('/login', methods = ['POST', 'GET'] )
 def login():
-    if request.method = 'POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=session['username']).first()
         if user and user.password == password:
             session['username'] = username
             flash("Logged in")
@@ -54,7 +54,44 @@ def login():
         else:
             flash('User password incorrect, or user does not exist', 'error')
     
-    return render_template('login.html')
+   #return render_template('login.html')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if not username or len(username) > 3 or len(username) > 20:
+            flash("Invalid username", "error")
+
+        elif not password or len(password) > 3 or len(password) > 20:
+            flash("Invalid password", "error")
+
+
+        elif password != verify:
+            flash("Not a valid password", "error")
+
+        else:
+            existing_user = User.query.filter_by(username=username).first()
+
+            if existing_user:
+                flash("User already exists", "error")
+            
+            else:
+                new_user = User(username, password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                return redirect('/newpost')
+
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/newpost')
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def index():
@@ -62,7 +99,7 @@ def index():
     if request.method == 'POST':
         title_name = request.form['title']
         body_name = request.form['body']
-        owner = User.query.filter_by(username=username).first() # Get owner from your session instead of owner field that doesen't exist.
+        owner = User.query.filter_by(username=session['username']).first() # Get owner from your session instead of owner field that doesen't exist.
 
         
 
